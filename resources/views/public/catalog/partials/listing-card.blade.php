650cff036c04f91b9a2299c2074fc91814db1f2d
kenng90 <kenngigz@yahoo.com>
2026-07-08 21:27:10 +0300

    fix listing checkout button issue

--- resources/views/public/catalog/partials/listing-card.blade.php
+++ resources/views/public/catalog/partials/listing-card.blade.php
@@ -99,7 +99,7 @@
                 type="button"
                 class="add-to-cart-btn"
                 style="background-color: #25D366;"
-                onclick="openBookingPanel('{{ $item['id'] }}', @json($item['title'] ?? 'Listing'))"
+                onclick="openBookingPanel(@js($item['id']), @js($item['title'] ?? 'Listing'))"
                 @if(in_array($statusValue, ['Sold', 'Leased', 'Reserved'], true)) disabled @endif
             >
                 @if(in_array($statusValue, ['Sold', 'Leased', 'Reserved'], true))
