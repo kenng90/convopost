1de09626f4be3cc1fb2b306c2b37f68f0c56806a
kenng90 <kenngigz@yahoo.com>
2026-09-21 21:52:38 +0300

    tik tok addition

---
+++ modules/Social/Database/Seeds/SocialDatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Social\Database\Seeds;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class SocialDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+
+        Model::reguard();
+    }
+}
